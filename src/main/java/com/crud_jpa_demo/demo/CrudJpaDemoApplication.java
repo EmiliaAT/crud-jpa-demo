cee01745fe19b3d14fc49d2bf6f2654b3148556e
EmiliaAT <latrickett@uncg.edu>
2025-05-30 17:39:48 -0400

    Project skeleton

---
+++ src/main/java/com/crud_jpa_demo/demo/CrudJpaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.csc340.crud_jpa_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudJpaDemoApplication.class, args);
+    }
+}
