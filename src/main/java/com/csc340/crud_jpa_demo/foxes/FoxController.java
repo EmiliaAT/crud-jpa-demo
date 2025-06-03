5391636cf5b19bfe18598a94a7fcaf4ad35a475c
EmiliaAT <latrickett@uncg.edu>
2025-06-02 23:39:08 -0400

    Add code documentation to Service and Controller

--- src/main/java/com/csc340/crud_jpa_demo/foxes/FoxController.java
+++ src/main/java/com/csc340/crud_jpa_demo/foxes/FoxController.java
@@ -13,22 +13,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * FoxController is a REST controller that handles HTTP requests related to
+ * foxes.
+ * 
+ * It provides endpoints for CRUD operations on fox data.
+ */
 @RestController
 public class FoxController {
 
     @Autowired
     private FoxService service;
 
+    /**
+     * Endpoint to get all foxes.
+     *
+     * @return List of all foxes.
+     */
     @GetMapping("/api/foxes")
     public List<Fox> getAllFoxes() {
         return this.service.getAllFoxes();
     }
 
+    /**
+     * Endpoint to get a fox by ID.
+     *
+     * @param id The ID of the fox to retrieve.
+     * @return The fox with the specified ID.
+     */
     @GetMapping("/api/foxes/{id}")
     public Optional<Fox> getFoxById(@PathVariable Long id) {
         return this.service.getFoxById(id);
     }
 
+    /**
+     * Endpoint to get foxes by name.
+     *
+     * @param name The name of the foxes to search for.
+     * @return List of foxes with a matching name.
+     */
     @GetMapping("/api/foxes/name")
     public List<Fox> getFoxesByName(@RequestParam Optional<String> key) {
         return key
@@ -36,6 +59,12 @@ public class FoxController {
                 .orElseGet(() -> this.service.getAllFoxes());
     }
 
+    /**
+     * Endpoint to get foxes by colour.
+     *
+     * @param colour The colour of the foxes to search for.
+     * @return List of foxes with the specified colour.
+     */
     @GetMapping("/api/foxes/colour")
     public List<Fox> getFoxesByColour(@RequestParam Optional<String> key) {
         return key
@@ -43,6 +72,12 @@ public class FoxController {
                 .orElseGet(() -> this.service.getAllFoxes());
     }
 
+    /**
+     * Endpoint to get foxes by species.
+     *
+     * @param colour The species of the foxes to search for.
+     * @return List of foxes with the specified species.
+     */
     @GetMapping("/api/foxes/species")
     public List<Fox> getFoxesBySpecies(@RequestParam Optional<String> key) {
         return key
@@ -50,6 +85,12 @@ public class FoxController {
                 .orElseGet(() -> this.service.getAllFoxes());
     }
 
+    /**
+     * Endpoint to get foxes by description.
+     *
+     * @param colour The description of the foxes to search for.
+     * @return List of foxes with the specified description.
+     */
     @GetMapping("/api/foxes/description")
     public List<Fox> getFoxesByDescription(@RequestParam Optional<String> key) {
         return key
@@ -57,16 +98,34 @@ public class FoxController {
                 .orElseGet(() -> this.service.getAllFoxes());
     }
 
+    /**
+     * Endpoint to add a new fox.
+     *
+     * @param fox The fox to add.
+     * @return The newly created fox.
+     */
     @PostMapping("/api/foxes")
     public Fox addFox(@RequestBody Fox fox) {
         return this.service.addFox(fox);
     }
 
+    /**
+     * Endpoint to update a fox.
+     *
+     * @param id  The ID of the fox to update.
+     * @param fox The updated fox information.
+     * @return The updated fox.
+     */
     @PutMapping("/api/foxes/{id}")
     public Fox updateFox(@PathVariable Long id, @RequestBody Fox fox) {
         return this.service.updateFox(id, fox);
     }
 
+    /**
+     * Endpoint to delete a fox.
+     *
+     * @param id The ID of the fox to delete.
+     */
     @DeleteMapping("/api/foxes/{id}")
     public void deleteFox(@PathVariable Long id) {
         this.service.deleteFox(id);
